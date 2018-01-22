71fff55592ab90ef46922341f4ab85ceb03d642a
arwn <windham.aren@protonmail.com>
2018-01-22 18:17:48 -0500

    almost works

--- C/caesar.c
+++ C/caesar.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
   int i, c;
   char input[MAXLEN] = "";
   int shift = 0;
-  
+
   /* set shift */
   if (argc > 1) {
     shift = atoi (argv[1]);
@@ -21,21 +21,29 @@ int main(int argc, char *argv[]) {
   if (argc > 2) {
     strcpy(input, argv[2]);
   } else {
-    for (i = 0; i < MAXLEN && (c=getchar()) != EOF; i++) {
-      input[i] = c > 126 ? c - 95 : c; // sane looping of chars
+    for (i = 0; i < MAXLEN - 1 && (c=getchar()) != EOF; i++) {
+      input[i] = c;
     }
     input[++i] = '\0';
   }
 
   encrypt(shift, &input[0]);
-
   printf("%s", input);
-  
+
 }
 
 void encrypt(int shift, char text[])
 {
-  while (*text != '\0') {
-    *text++ += shift;
+  while (*text) {
+    *text += shift;
+
+    while (*text > 126) {
+      *text = *text - 95;
+    }
+    while (*text < 32) {
+      *text = *text + 95;
+    }
+    
+    text++;
   }
 }
