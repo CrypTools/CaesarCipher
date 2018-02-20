1db8a8e1dc78bf1bb9fd02767517a48f463f2e74
Arthur Guiot <arguiot@gmail.com>
2018-02-20 12:35:19 +0800

    Fixed Swift tests

--- __test__/test.swift
+++ __test__/test.swift
@@ -29,20 +29,20 @@ extension String {
 
 func test() -> Int {
     print("Testing Encryption")
-    if "Hello World!".encrypt(n: 4) == "Lipps$[svph%" {
+    if "Hello World!".encrypt(4) == "Lipps$[svph%" {
         print("Done! No problems")
     } else {
-        print("Oups!\n \"Hello World!\".encrypt(n: 4) isn't equal to \"Lipps$[svph%\"")
-        print("Output: \("Hello World!".encrypt(n: 4))")
+        print("Oups!\n \"Hello World!\".encrypt(4) isn't equal to \"Lipps$[svph%\"")
+        print("Output: \("Hello World!".encrypt(4))")
         exit(1)
     }
 
     print("Testing Decryption")
-    if "Lipps$[svph%".decrypt(n: 4) == "Hello World!" {
+    if "Lipps$[svph%".decrypt(4) == "Hello World!" {
         print("Done! No problems")
     } else {
-        print("Oups!\n \"Lipps$[svph%\".decrypt(n: 4) isn't equal to \"Hello World!\"")
-        print("Output: \("Lipps$[svph%".decrypt(n: 4))")
+        print("Oups!\n \"Lipps$[svph%\".decrypt(4) isn't equal to \"Hello World!\"")
+        print("Output: \("Lipps$[svph%".decrypt(4))")
         exit(1)
     }
     print("Tests are done.")
