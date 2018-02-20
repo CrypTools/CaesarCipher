c491ce463e4de75623100e914fd8dc8229488feb
Arthur Guiot <arguiot@gmail.com>
2018-02-20 12:31:28 +0800

    Simplified Swift Lib

--- swift/lib.swift
+++ swift/lib.swift
@@ -1,37 +1,25 @@
 /*********************************
-*
-* Use: "Hello World!".encrypt(n: 4)
-* => "Lipps$[svph%"
-*      "Lipps$[svph%".decrypt(n: 4)
-* => "Hello World!"
-*
-*********************************/
-import Foundation
-
-// Get array of ascii values from string
+ *
+ * Use: "Hello World!".encrypt(4)
+ * => "Lipps$[svph%"
+ *      "Lipps$[svph%".decrypt(4)
+ * => "Hello World!"
+ *
+ *********************************/
 extension String {
-    var asciiArray: [UInt32] {
-        return unicodeScalars.filter{$0.isASCII}.map{$0.value}
-    }
-}
-extension Character {
-    var asciiValue: UInt32? {
-        return String(self).unicodeScalars.filter{$0.isASCII}.first?.value
-    }
-}
-// main
-extension String {
-    func encrypt(n: Int) -> String {
+    func encrypt(_ n: Int) -> String {
         var out = "";
-        for i in self {
-            out += String(Character(UnicodeScalar(Int(i.asciiValue!) + n)!))
+        let unicode = Array(self.unicodeScalars.map { $0.value }).map { Int($0) }
+        for i in unicode {
+            out += String(Character(UnicodeScalar(i + n)!))
         }
         return out
     }
-    func decrypt(n: Int) -> String {
+    func decrypt(_ n: Int) -> String {
         var out = "";
-        for i in self {
-            out += String(Character(UnicodeScalar(Int(i.asciiValue!) - n)!))
+        let unicode = Array(self.unicodeScalars.map { $0.value }).map { Int($0) }
+        for i in unicode {
+            out += String(Character(UnicodeScalar(i - n)!))
         }
         return out
     }
