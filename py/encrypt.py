ccd171755cd741ff31e4c8bae1b0e1dc9bc65c3a
Vedant Bhavsar <50816305+Exlaso@users.noreply.github.com>
2023-08-13 18:19:03 +0530

    Update encrypt.py
    Encrypt Message With the Information of Lower case and Upper Case

--- py/encrypt.py
+++ py/encrypt.py
@@ -1,16 +1,12 @@
-LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-
-def encrypt(initial, shift):
-  """ Use : encrypt("message", 98)
-  => 'gymmuay'
-  
-  Encrypts text using Caesar Cipher
-  """
-  initial = initial.lower()
-  output = ""
-
-  for char in initial:
-    if char in LETTERS:
-      output += LETTERS[(LETTERS.index(char) + shift) % len(LETTERS)]
-
-  return output
+def encrypt(val,key):
+    smallalpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    bigalpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    retval = ''
+    for chr in val:
+        if chr.isupper(): 
+            chr_index = bigalpha.index(chr) 
+            retval = retval+bigalpha[(chr_index + key) % 26]
+        else: 
+            chr_index = smallalpha.index(chr)
+            retval = retval+smallalpha[(chr_index + key) % 26]
+    return retval;         
